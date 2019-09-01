73f637faec9581a5b8d5a6a596ee040777e085d6
beaudu <beauducel@gmail.com>
2019-09-01 11:51:52 +0700

    first tentative of vectorized CDM + README update

--- nikkhoo/README.md
+++ nikkhoo/README.md
@@ -1,38 +1,71 @@
-# pCDM: point Compound Dislocation Model
-
+# CDM: Compound Dislocation Model
 
 *Nikkhoo et al.* [2017] model calculates analytical solution for surface displacements due to a combination of three mutually orthogonal tensile displocations (one horizontal and two vertical) freely oriented in space (three angles of rotation) in an elastic half-space.
 
-This model can be used to simulate inflation or deflation of a volumetric source observed in the far field, like magmatic instrusion under a volcano and GPS surface stations measurements. The model is able to approximate any shape: isotropic, sill, dyke, pipe, or any ellipsoid in any orientations in space. In practice, this code is able to replace, in the far-field, former [Mogi](../mogi), [Sun](../sun), [Okada](../okada), Davis, Bonaccorso, and other analytic models of simple sources in elastic half-space that have been proposed in the last century.
+This model can be used to simulate inflation or deflation of a volumetric source, like magmatic instrusion under a volcano and GPS stations measurements at surface. The model is able to approximate the most common shapes of source: isotropic, sill, dyke, pipe, or any ellipsoid in any orientations in space. In practice, this code is able to replace, especially in the far-field, former [Mogi](../mogi), [Sun](../sun), [Okada](../okada), Davis, Bonaccorso, and other analytic models of simple sources in elastic half-space that have been proposed in the last century.
+
+Nikkhoo has proposed two versions of analytical solutions (see [www.volcanodeformation.com](http://www.volcanodeformation.com)):
+
+* the **CDM** is composed of 3 finite rectangular dislocations of different semi-axe lengths, all with the same amount of opening. The source is fully described by a total of 10 parameters;
+* the **pCDM** is composed of 3 point tensile dislocations of different potencies (volume variations). The equations are simplier and much faster. Source is fully described by only 9 parameters. This is a good model for far-field observations.
+
+<figure style="text-align: center; margin: 3em">
+  <img src="Nikkhoo2017_CDM_fig3d.png" alt="CDM"/>
+  <figcaption><em>Figure 1. The three rectangular discontinuities composing the CDM and rotation angle definition (from Nikkhoo et al., 2017).</em></figcaption>
+</figure>
+
+
+The proposed scripts are literal transcriptions of the Nikkhoo's equations from original script `CDM.m` and `pCDM.m`, except for:
+
+- the source coordinates is set to (0,0) so observation points (x,y) are relative to it;
+- the equations have been vectorized for all input source parameters. So massive computation can be achieved on large vectors, matrix or even N-D matrix of inputs, which is useful for inversion.
+
+## CDM code
 
 |Input arguments|Description|
 |-------------:|:----------|
-|X and Y| Horizontal coordinates (East, North) of calculation points relative to source located at (0,0).|
-|DEPTH  | Depth of the source from calculation points, same unit as X and Y. Note that you might add source depth to elevation at each calculation points to approximate the topographic effects.|
+|X and Y| Horizontal coordinates (East, North) of calculation points relative to source located at (0,0). For original code `CDM.m` it corresponds to X-X0 and Y-Y0, respectively.|
+|DEPTH  | Depth of the source from calculation points, same unit as X and Y. Note that you might add the elevation at each calculation points to approximate the topographic effects.|
+|OMEGAX OMEGAY OMEGAZ| Clockwise rotation angles around X, Y and Z axes, respectively, that specify the orientation of the CDM in space, in degrees.|
+|AX AY AZ| Semi-axes of the CDM along the X, Y and Z axes, respectively, before applying the rotations. AX, AY and AZ have the same unit as X and Y.|
+|OPENING | The opening (tensile component of the Burgers vector) of the rectangular dislocation that form the CDM. Same unit as AX, AY and AZ.|
+|NU | Poisson's ratio, optional and dimensionless (default is 0.25 for an isotropic medium).|
+
+
+### cdmv.m
+
+Pure Matlab/GNU Octave code fully vectorized (no loop, no if condition). Type "doc cdmv" for help, syntax and example, and see script comments for details.
+
+
+## pCDM codes
+
+For this code we introduced a difference from original function to have the total potency as one of the input parameter: the 3 volume potencies have been redefined as a total volume variation *dVtot* and two dimensionless shape parameters *A* and *B*, between 0 and 1, defined as in the upper table (see Figure 2 for examples).
+
+<figure style="text-align: center; margin: 3em">
+  <img src="pcdm_ab.png" alt="pCDM"/>
+  <figcaption><em>Figure 2. Example of source shapes that can be simulated by pCDM. On this figure surface of each point dislocation is enlarged to be proportional to its associated volume variation.</em></figcaption>
+</figure>
+
+|Input arguments|Description|
+|-------------:|:----------|
+|X and Y| Horizontal coordinates (East, North) of calculation points relative to source located at (0,0). For original code `pCDM.m` it corresponds to X-X0 and Y-Y0, respectively.|
+|DEPTH  | Depth of the source from calculation points, same unit as X and Y. Note that you might add the elevation at each calculation points to approximate the topographic effects.|
 |OMEGAX OMEGAY OMEGAZ| Clockwise rotation angles around X, Y and Z axes, respectively, that specify the orientation of the pCDM in space, in degrees.|
 |DV| Total potency DV = dVX+dVY+dVZ of the point tensile dislocations that before applying the rotations are normal to the X, Y and Z axes, respectively. The potency has the unit of volume (the unit of displacements and semi-axes to the power of 3).|
 |A | Horizontal over total volume variation ratio A = dVZ/DV.|
 |B | Vertical volume variation ratio B = dVY/(dVX+dVY).|
 |NU | Poisson's ratio, optional and dimensionless (default is 0.25 for an isotropic medium).|
 
-## pcdmv.m
-The proposed Matlab script is a literal transcription of the Nikkhoo's equations from original script pCDM.m (see [www.volcanodeformation.com](http://www.volcanodeformation.com)), except for:
+### pcdmv.m
 
-- the source coordinates is set to (0,0) so observation points (x,y) are relative to it;
-- the volume potencies have been redefined as a total volume variation *dVtot* and two dimensionless shape parameters *A* and *B*, between 0 and 1, defined as in the upper table (see Figure 1 for examples);
-- the equations have been vectorized for all input parameters. So massive computation can be achieved on large vectors, matrix or even N-D matrix of inputs which is useful for inversion.
+Pure Matlab/GNU Octave code fully vectorized (no loop, no if condition). Type "doc pcdmv" for help, syntax and example, and see script comments for details.
 
-![](pcdm_ab.png)
-*Figure 1. Example of source shapes that can be simulated by pCDM. On this figure surface of each point dislocation is enlarged to be proportional to its associated volume variation.*
-
-Type "doc pcdmv" for help, syntax and example, and see script comments for details.
-
-## pcdmv.c
+### pcdmv.c
 This is a transcription of `pcdmv.m` in C language that includes complementary subfunction `mexFunction()` to be compiled as a MEX file (Matlab/Octave executable). To make the binary for your computer architecture, you must install a compiler first then type at the Matlab/Octave command line:
 
 	>> mex pcdmv.c
 
-## pcdmdesc.m
+### pcdmdesc.m
 A small script to transform source shape parameters *A* and *B* to a human-readable string. It uses a default 10% tolerancy. Examples:
 
 	>> pcdmdesc(1,0)
@@ -46,29 +79,34 @@ A small script to transform source shape parameters *A* and *B* to a human-reada
 
 Type "doc pcdmdesc" for help, syntax and examples.
 
-## plotpcdm.m
+### plotpcdm.m
 A first tentative script to represent a pCDM source on a graph, in 3-D or 2-D projections, from shape parameters A and B.
 
 ## Notes on vectorization
-The three codes have almost the same input parameters but different behaviors with input vectors and matrix:
+The codes have almost the same input parameters but different behaviors with input vectors and matrix:
 
-### original pCDM.m by Nikkhoo (2016)
-It accepts vectors or matrix only for X and Y observation points coordinates. Other input parameters X0, Y0, DEPTH, OMEGAX, OMEGAY, OMEGAZ, DVX, DVY, DVZ, and NU must be scalars.
+### Originals by Nikkhoo (2016)
 
-### pcdmv.m
-Accepts scalar, vector or matrix of the same size for all input parameters X, Y, DEPTH, OMEGAX, OMEGAY, OMEGAZ, DV, A, B, and optional NU, and any of them can be also a scalar, excepted for X: since output arguments will be set to the same size as X, if X is a scalar and other input arguments are vectors or matrix, you must use `repmat(X,...)` to make X also a vector or matrix. For other input arguments, any mixing between matrix and scalars are acceptable (if all matrix have the same size, of course).
+**CDM.m** accepts vectors or matrix only for X and Y observation points coordinates. Other input parameters defining the source X0, Y0, DEPTH, OMEGAX, OMEGAY, OMEGAZ, AX, AY, AZ, OPENING and NU must be scalars.
 
-### pcdmv.c
-Accepts all input arguments X, Y, DEPTH, OMEGAX, OMEGAY, OMEGAZ, DV, A, and B   as scalar, vector or matrix but all must have the same number of elements or size. Mixing with scalar is prohibited so you may use `repmat()` to convert any scalar to a matrix if necessary. Last input argument NU is mandatory and must be a scalar. Output arguments will be set to the size of input arguments.
+**pCDM.m** accepts vectors or matrix only for X and Y observation points coordinates. Other input parameters defining the source X0, Y0, DEPTH, OMEGAX, OMEGAY, OMEGAZ, DVX, DVY, DVZ, and NU must be scalars.
 
-### performance
-This a basic comparison of computational time for one million different random models with constant NU = 0.25, using a 2.7GHz Intel Core i7 computer.
+### Vectorized Matlab/GNU Octave
+**cdmv.m** accepts scalar, vector or matrix of the same size for all input parameters X, Y, DEPTH, OMEGAX, OMEGAY, OMEGAZ, AX, AY, AZ, OPENING, and optional NU, and any of them can be also a scalar, excepted for X: since output arguments will be set to the same size as X, if X is a scalar and other input arguments are vectors or matrix, you must use `repmat(X,...)` to make X also a vector or matrix. For other input arguments, any mixing between matrix and scalars are acceptable (if all matrix have the same size, of course).
 
-|code|time|
-|:----|--------|
-|pCDM.m (original)|4.0 s|
-|pcdmv.m|1.0 s|
-|pcdmv.c (compiled)|0.4 s|
+**pcdmv.m** accepts scalar, vector or matrix of the same size for all input parameters X, Y, DEPTH, OMEGAX, OMEGAY, OMEGAZ, DV, A, B, and optional NU, and any of them can be also a scalar, excepted for X: since output arguments will be set to the same size as X, if X is a scalar and other input arguments are vectors or matrix, you must use `repmat(X,...)` to make X also a vector or matrix. For other input arguments, any mixing between matrix and scalars are acceptable (if all matrix have the same size, of course).
+
+### Compiled C as MEX/OCT
+**pcdmv.c** accepts all input arguments X, Y, DEPTH, OMEGAX, OMEGAY, OMEGAZ, DV, A, and B   as scalar, vector or matrix but all must have the same number of elements or size. Mixing with scalar is prohibited so you may use `repmat()` to convert any scalar to a matrix if necessary. Last input argument NU is mandatory and must be a scalar. Output arguments will be set to the size of input arguments.
+
+### Performance
+This a basic comparison of computational times using a 2.7GHz Intel Core i7 computer for 100,000 different random models computed for 10 observation points. All with constant NU = 0.25. Since the original scripts where vectorized for observation points only, the gain is mostly on the source parameters.
+
+|code| CDM| pCDM|
+|----:|--------:|-------:|
+|original .m|189 s|8.0 s|
+|vectorized .m|6 s|0.9 s|
+|compiled .c|-|0.4 s|
 
 ## Rerefences
 
---
+++ nikkhoo/cdmv.m
@@ -0,0 +1,328 @@
+function varargout=cdmv(varargin)
+%CDMV	Compound Dislocation Model
+%	[uE,uN,uV,DV]=CDMV(X,Y,DEPTH,OMEGAX,OMEGAY,OMEGAZ,AX,AY,AZ,OPEN,NU)
+%	calculates the surface displacements and potency associated with a
+%	source composed of three mutually orthogonal rectangular dislocations
+%	in a half-space (Nikkhoo et al., 2017).
+%
+%	This model is able to approximate displacements due to various shapes
+%	of source like isotropic, sill, dyke, pipe, or any ellipsoid in any 
+%	orientations in space.
+%
+%	Equations have been fully vectorized so all input parameters can be
+%	vectors or N-D matrix of the same number of elements, and any of them
+%	can be a scalar with some restrictions described below. Output 
+%	parameters will have the same size as X.
+%
+%
+%	--- Input parameters
+%
+%	X and Y:
+%	   Horizontal coordinates (East, North) of calculation points relative
+%	   to source located at (0,0). If X is a scalar, all other input 
+%	   parameters must be also scalars. Use repmat(X,...) to convert X to
+%	   a vector/matrix if necessary.
+%
+%	DEPTH:
+%	   Depth of the source of the CDM centroid. The depth must be a positive 
+%	   value and have the same unit as X and Y.
+%
+%	OMEGAX, OMEGAY and OMEGAZ:
+%	   Clockwise rotation angles about X, Y and Z axes, respectively, that 
+%	   specify the orientation of the CDM in space, in degrees. The three
+%	   angles must have the same number of elements, i.e., mixing of scalar
+%	   and vector/matrix is not allowed.
+%
+%	AX, AY and AZ:
+%	   Semi-axes of the CDM along the X, Y and Z axes, respectively, before
+%	   applying the rotations. Must have the same unit as X and Y.
+%
+%	OPEN:
+%	   The opening (tensile component of the Burgers vector) of the RDs 
+%	   that form the CDM. Must be the same as the unit of X and Y.
+%
+%	NU:
+%	   Poisson's ratio, optional and dimensionless (default is 0.25 for 
+%	   an isotropic medium).
+% 
+% 
+%	--- Outputs parameters
+%
+%	uE, uN and uV:
+%	   Calculated displacement vector components in EFCS. Will have the
+%	   same unit as OPEN and the CDM semi-axes in inputs.
+%
+%	DV:
+%	   Potency of the CDM. DV has the unit of volume, i.e. the unit of 
+%	   displacements, opening and CDM semi-axes to the power of 3.
+%
+% 
+%	--- Glossary
+%
+%	 CDM: Compound Dislocation Model
+%	  RD: Rectangular Dislocation
+%	EFCS: Earth-Fixed Coordinate System
+%	RDCS: Rectangular Dislocation Coordinate System
+%	      The origin of the RDCS is the RD centroid. The axes of the RDCS 
+%	      are aligned with the strike, dip and normal vectors of the RD,
+%	      respectively.
+%	ADCS: Angular Dislocation Coordinate System
+%
+%
+%	--- References
+%
+%	   Nikkhoo, M., Walter, T. R., Lundgren, P. R., Prats-Iraola, P. (2017):
+%	    Compound dislocation models (CDMs) for volcano deformation analyses.
+%	    Geophys. J. Int., 208(2): 877-894, doi:10.1093/gji/ggw427
+%
+%	   http://www.volcanodeformation.com
+%
+%
+%	Authors: François Beauducel, Antoine Villié, and Mehdi Nikkhoo
+%	Created: 2015-05-22 in GFZ Potsdam (Germany) by Mehdi Nikkhoo
+%	Updated: 2019-08-31
+
+%	Copyright (c) 2016 Mehdi Nikkhoo
+%	Copyright (c) 2019 François Beauducel
+%
+%	Permission is hereby granted, free of charge, to any person obtaining a
+%	copy of this software and associated documentation files
+%	(the "Software"), to deal in the Software without restriction, including
+%	without limitation the rights to use, copy, modify, merge, publish,
+%	distribute, sublicense, and/or sell copies of the Software, and to permit
+%	persons to whom the Software is furnished to do so, subject to the
+%	following conditions:
+%
+%	The above copyright notice and this permission notice shall be included
+%	in all copies or substantial portions of the Software.
+%
+%	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+%	OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+%	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+%	NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+%	DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+%	OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+%	USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+% checks input arguments
+if length(find(unique(cellfun(@numel,varargin)) ~= 1)) > 1
+	error('All input arguments must be scalars or have the same numel or elements.')
+end
+sx = size(varargin{1});
+
+% inputs arguments are converted to column vectors (linear indexing)
+X  = varargin{1}(:);
+Y  = varargin{2}(:);
+D  = varargin{3}(:);
+OX = varargin{4}(:);
+OY = varargin{5}(:);
+OZ = varargin{6}(:);
+AX = 2*abs(varargin{7}(:));
+AY = 2*abs(varargin{8}(:));
+AZ = 2*abs(varargin{9}(:));
+OP = varargin{10}(:);
+if nargin < 11
+	NU = 0.25;
+else
+	NU = varargin{11}(:);
+end
+
+% coefficients from the 3-D matrix of rotation
+R11 =  cosd(OY).*cosd(OZ);
+R12 = -cosd(OY).*sind(OZ);
+R13 =  sind(OY);
+R21 =  cosd(OZ).*sind(OX).*sind(OY) + cosd(OX).*sind(OZ);
+R22 = -sind(OX).*sind(OY).*sind(OZ) + cosd(OX).*cosd(OZ);
+R23 = -sind(OX).*cosd(OY);
+R31 = -cosd(OX).*sind(OY).*cosd(OZ) + sind(OX).*sind(OZ);
+R32 =  cosd(OX).*sind(OY).*sind(OZ) + sind(OX).*cosd(OZ);
+R33 =  cosd(OX).*cosd(OY);
+
+% coordinates for each RD summits
+P1 = [AY.*R21/2 + AZ.*R31/2, AY.*R22/2 + AZ.*R32/2, AY.*R23/2 + AZ.*R33/2 - D];
+P2 = P1 - repmat(AY,1,3).*[R21, R22, R23];
+P3 = P2 - repmat(AZ,1,3).*[R31, R32, R33];
+P4 = P1 - repmat(AZ,1,3).*[R31, R32, R33];
+
+Q1 = [-AX.*R11/2 + AZ.*R31/2, -AX.*R12/2 + AZ.*R32/2, -AX.*R13/2 + AZ.*R33/2 - D];
+Q2 = Q1 + repmat(AX,1,3).*[R11, R12, R13];
+Q3 = Q2 - repmat(AZ,1,3).*[R31, R32, R33];
+Q4 = Q1 - repmat(AZ,1,3).*[R31, R32, R33];
+
+R1 = [AX.*R11/2 + AY.*R21/2, AX.*R12/2 + AY.*R22/2, AX.*R13/2 + AY.*R23/2 - D];
+R2 = R1 - repmat(AX,1,3).*[R11, R12, R13];
+R3 = R2 - repmat(AY,1,3).*[R21, R22, R23];
+R4 = R1 - repmat(AY,1,3).*[R21, R22, R23];
+
+[ue1,un1,uv1] = RDdispSurf(X,Y,P1,P2,P3,P4,OP,NU);
+[ue2,un2,uv2] = RDdispSurf(X,Y,Q1,Q2,Q3,Q4,OP,NU);
+[ue3,un3,uv3] = RDdispSurf(X,Y,R1,R2,R3,R4,OP,NU);
+
+ue = ue1+ue2+ue3;
+un = un1+un2+un3;
+uv = uv1+uv2+uv3;
+
+% special cases (one dimension is zero)
+k = (AX==0 & AY~=0 & AZ~=0);
+ue(k) = ue1(k);
+un(k) = un1(k);
+uv(k) = uv1(k);
+
+k = (AX~=0 & AY==0 & AZ~=0);
+ue(k) = ue2(k);
+un(k) = un2(k);
+uv(k) = uv2(k);
+
+k = (AX~=0 & AY~=0 & AZ==0);
+ue(k) = ue3(k);
+un(k) = un3(k);
+uv(k) = uv3(k);
+
+% special cases (two or three dimensions are zero)
+k = all([AX,AY,AZ]==0,2) | all([AX,AZ]==0,2) | all([AX,AZ]==0,2) | all([AY,AZ]==0,2);
+ue(k) = 0;
+un(k) = 0;
+uv(k) = 0;
+
+% half-space solution: The CDM must be under the free surface!
+kair = any([P1(:,3) P2(:,3) P3(:,3) P4(:,3) ...
+            Q1(:,3) Q2(:,3) Q3(:,3) Q4(:,3) ...
+            R1(:,3) R2(:,3) R3(:,3) R4(:,3)] > 0,2);
+ue(kair) = NaN;
+un(kair) = NaN;
+uv(kair) = NaN;
+
+% reshapes outputs to the original size of X
+varargout{1} = reshape(ue,sx);
+varargout{2} = reshape(un,sx);
+varargout{3} = reshape(uv,sx);
+
+% Calculate the CDM total potency (AX, AY and AZ were converted to full axes)
+if nargout > 3
+	varargout{4} = reshape((AX.*AY + AX.*AZ + AY.*AZ).*OP,sx);
+end
+
+end
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [ue,un,uv]=RDdispSurf(X,Y,P1,P2,P3,P4,OP,nu)
+% RDdispSurf calculates surface displacements associated with a rectangular
+% dislocation in an elastic half-space.
+
+Vnorm = cross(P2-P1,P4-P1,2);
+Vnorm = Vnorm./repmat(sqrt(sum(Vnorm.^2,2)),1,3);
+bX = OP.*Vnorm(:,1);
+bY = OP.*Vnorm(:,2);
+bZ = OP.*Vnorm(:,3);
+
+[u1,v1,w1] = AngSetupFSC(X,Y,bX,bY,bZ,P1,P2,nu); % Side P1P2
+[u2,v2,w2] = AngSetupFSC(X,Y,bX,bY,bZ,P2,P3,nu); % Side P2P3
+[u3,v3,w3] = AngSetupFSC(X,Y,bX,bY,bZ,P3,P4,nu); % Side P3P4
+[u4,v4,w4] = AngSetupFSC(X,Y,bX,bY,bZ,P4,P1,nu); % Side P4P1
+
+ue = u1 + u2 + u3 + u4;
+un = v1 + v2 + v3 + v4;
+uv = w1 + w2 + w3 + w4;
+
+end
+
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [X1,X2,X3]=CoordTrans(x1,x2,x3,A1,A2,A3)
+% CoordTrans transforms the coordinates of the vectors, from
+% x1x2x3 coordinate system to X1X2X3 coordinate system. The transformation
+% whose columns A1, A2 and A3 are the unit base vectors of the x1x2x3. The
+% coordinates of e1,e2 and e3 in A must be given in X1X2X3. The transpose
+% of A (i.e., A') will transform the coordinates from X1X2X3 into x1x2x3.
+
+X1 = A1(:,1).*x1 + A1(:,2).*x2 + A1(:,3).*x3;
+X2 = A2(:,1).*x1 + A2(:,2).*x2 + A2(:,3).*x3;
+X3 = A3(:,1).*x1 + A3(:,2).*x2 + A3(:,3).*x3;
+end
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [ue,un,uv]=AngSetupFSC(X,Y,bX,bY,bZ,PA,PB,nu)
+% AngSetupSurf calculates the displacements associated with an angular
+% dislocation pair on each side of an RD in a half-space.
+
+SideVec = PB-PA;
+beta = acos(-SideVec(:,3)./sqrt(sum(SideVec.^2,2)));
+
+ey1 = [SideVec(:,1:2),zeros(size(X))];
+ey1 = ey1./repmat(sqrt(sum(ey1.^2,2)),1,3);
+ey3 = repmat([0 0 -1],length(X),1);
+ey2 = cross(ey3,ey1,2);
+
+% Transform coordinates from EFCS to the first ADCS
+[y1A,y2A,~] = CoordTrans(X-PA(:,1),Y-PA(:,2),-PA(:,3),ey1,ey2,ey3);
+% Transform coordinates from EFCS to the second ADCS
+[y1AB,y2AB,~] = CoordTrans(SideVec(:,1),SideVec(:,2),SideVec(:,3),ey1,ey2,ey3);
+y1B = y1A-y1AB;
+y2B = y2A-y2AB;
+
+% Transform slip vector components from EFCS to ADCS
+[b1,b2,b3] = CoordTrans(bX,bY,bZ,ey1,ey2,ey3);
+
+[v1A,v2A,v3A] = AngDisDispSurf(y1A,y2A,beta,b1,b2,b3,nu,-PA(:,3));
+[v1B,v2B,v3B] = AngDisDispSurf(y1B,y2B,beta,b1,b2,b3,nu,-PB(:,3));
+
+% artefact-free for the calculation points near the free surface
+I = (beta.*y1A)>=0;
+if any(I)
+    [v1A(I),v2A(I),v3A(I)] = AngDisDispSurf(y1A(I),y2A(I),beta(I)-pi,b1(I),b2(I),b3(I),nu,-PA(I,3));
+    [v1B(I),v2B(I),v3B(I)] = AngDisDispSurf(y1B(I),y2B(I),beta(I)-pi,b1(I),b2(I),b3(I),nu,-PB(I,3));
+end
+
+% Calculate total displacements in ADCS
+v1 = v1B - v1A;
+v2 = v2B - v2A;
+v3 = v3B - v3A;
+
+% Transform total displacements from ADCS to EFCS
+ue = ey1(:,1).*v1 + ey2(:,1).*v2 + ey3(:,1).*v3;
+un = ey1(:,2).*v1 + ey2(:,2).*v2 + ey3(:,2).*v3;
+uv = ey1(:,3).*v1 + ey2(:,3).*v2 + ey3(:,3).*v3;
+
+k = abs(beta)<eps | abs(pi-beta)<eps;
+ue(k) = 0;
+un(k) = 0;
+uv(k) = 0;
+
+end
+
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [v1,v2,v3] = AngDisDispSurf(y1,y2,beta,b1,b2,b3,nu,a)
+% AngDisDispSurf calculates the displacements associated with an angular
+% dislocation in a half-space.
+
+sinB = sin(beta);
+cosB = cos(beta);
+cotB = cot(beta);
+z1 = y1.*cosB + a.*sinB;
+z3 = y1.*sinB - a.*cosB;
+r = sqrt(y1.^2 + y2.^2 + a.^2);
+
+Fi = 2*atan2(y2,(r+a).*cot(beta/2) - y1); % The Burgers function
+
+v1b1 = b1/2/pi.*((1-(1-2*nu).*cotB.^2).*Fi + y2./(r+a).*((1-2*nu).*(cotB + y1/2./(r+a)) - y1./r) - y2.*(r.*sinB - y1).*cosB./r./(r-z3));
+v2b1 = b1/2/pi.*((1-2*nu).*((.5+cotB.^2).*log(r+a)-cotB./sinB.*log(r-z3)) - 1./(r+a).*((1-2*nu).*(y1.*cotB - a/2 - y2.^2./2./(r+a)) + y2.^2./r) + y2.^2.*cosB./r./(r-z3));
+v3b1 = b1/2/pi.*((1-2*nu).*Fi.*cotB + y2./(r+a).*(2*nu + a./r) - y2.*cosB./(r-z3).*(cosB + a./r));
+
+v1b2 = b2/2/pi.*(-(1-2*nu).*((.5-cotB.^2).*log(r+a) + cotB.^2.*cosB.*log(r-z3))-1./(r+a).*((1-2*nu).*(y1.*cotB + .5*a + y1.^2./2./(r+a))-y1.^2./r) + z1.*(r.*sinB-y1)./r./(r-z3));
+v2b2 = b2/2/pi.*((1+(1-2*nu).*cotB.^2).*Fi-y2./(r+a).*((1-2*nu).*(cotB + y1/2./(r+a))- y1./r) - y2.*z1./r./(r-z3));
+v3b2 = b2/2/pi.*(-(1-2*nu).*cotB.*(log(r+a) - cosB.*log(r-z3)) - y1./(r+a).*(2*nu + a./r) + z1./(r-z3).*(cosB + a./r));
+
+v1b3 = b3/2/pi.*(y2.*(r.*sinB - y1).*sinB./r./(r-z3));
+v2b3 = b3/2/pi.*(-y2.^2.*sinB./r./(r-z3));
+v3b3 = b3/2/pi.*(Fi + y2.*(r.*cosB + a).*sinB./r./(r-z3));
+
+v1 = v1b1 + v1b2 + v1b3;
+v2 = v2b1 + v2b2 + v2b3;
+v3 = v3b1 + v3b2 + v3b3;
+
+end
